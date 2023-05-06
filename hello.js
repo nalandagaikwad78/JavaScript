03bca82750708df3a31a76908b4e894e698eafcf
nalandagaikwad78 <nalandagaikwad78@gmail.com>
2023-05-06 07:29:19 +0530

    new js
    new js

---
+++ hello.js
@@ -0,0 +1,28 @@
+
+
+
+
+console.log("Nalanda Gaikwad");
+console.log("At.Post.Peth Sangvi,Ta.Omerga,Dist.Dharashiv") 
+
+
+var firstName = "Nalanda";
+console.log(firstName); 
+
+
+var age;//variable declearation
+age =22;// variable initionalazation
+console.log(age);
+
+var holder;//variable declearation
+console.log(holder);//undefined
+holder = "my spac";//variable initionalazation
+console.log(holder);
+
+holder = "sachin spac";//variable update
+console.log(holder);
+
+var int12 ;
+console.log(int12);
+int12 = "G-tech";
+console.log(int12);
