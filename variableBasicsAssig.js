675dce3f5fd414a6f88edd947baadb1c8b4d60d6
nalandagaikwad78 <nalandagaikwad78@gmail.com>
2023-05-08 07:42:16 +0530

    practice javascript
    practice javascript

---
+++ variableBasicsAssig.js
@@ -0,0 +1,42 @@
+//Number//
+var PIN;
+PIN=410564
+console.log(PIN);
+
+var typeOfPin = typeof PIN;
+console.log(typeOfPin);
+
+
+//STRING---> Sequence of character enclosed in '' or ""
+var Village = "Pashan";
+Village = "Pashan";
+var typeOfVillage = typeof Village;
+console.log("Type of Village ->", typeof Village);
+
+//Boolean ---> true or false
+var knowJavaScript = false;
+
+var isSoftwareEngineer = true;
+var typeOfIsSoftwareEngineer = typeof isSoftwareEngineer;
+console.log("Type of isSoftwareEngineer:", typeOfIsSoftwareEngineer );
+
+
+
+//2. Special Data type
+// 1. Undefined
+var name;
+console.log("name:" , name);
+console.log("type of variable name is;", typeof name);
+
+
+// 2. null  
+var rollNo = null;
+console.log("value of variable ---> rollNo:",rollNo);
+console .log("type of variable rollNo is ", typeof rollNo);
+
+
+
+
+
+
+
