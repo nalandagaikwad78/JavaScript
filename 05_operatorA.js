67480c14d3c16801cbda4ddd34830043d11b7f1d
nalandagaikwad78 <nalandagaikwad78@gmail.com>
2023-05-21 21:34:00 +0530

    Operator A
    Operator A

---
+++ 05_operatorA.js
@@ -0,0 +1,70 @@
+
+
+function squareOfWordLength(){
+console.log("The square of lenght of word");
+}
+squareOfWordLength();
+
+
+var greet = "JavaScript";
+var resultSplit = greet.split(" ");
+console.log(resultSplit);
+console.log("Length of words in string is: ",resultSplit.length);
+
+
+var square = function (one) {
+    var result = 1*1;
+    return `Square words in string is: ${result}`;
+}
+var result = square(1 );
+console.log(result);
+
+var greet = "Google Chrome";
+var resultSplit = greet.split(" ");
+console.log(resultSplit);
+console.log("Length of words in string is: ",resultSplit.length);
+
+
+var square = function (two) {
+    var result = 2*2;
+    return `Square words in string is: ${result}`;
+}
+var result = square(2 );
+console.log(result);
+
+var greet = "Developer Smart";
+var resultSplit = greet.split(" ");
+console.log(resultSplit);
+console.log("Length of words in string is: ",resultSplit.length);
+
+
+var square = function (two) {
+    var result = 2*2;
+    return `Square words in string is: ${result}`;
+}
+var result = square(2 );
+console.log(result);
+
+function squareOfWordLength(){
+    console.log("I am Angular Devloper");
+    }
+    squareOfWordLength();
+
+    var greet = "I am Angular Devloper";
+var resultSplit = greet.split(" ");
+console.log(resultSplit);
+console.log("Length of words in string is: ",resultSplit.length);
+
+var greetLength = greet.length;
+console.log("Total count of string words is: ", greetLength);
+
+
+
+var num1 = 21;
+var num2 = 4;
+
+var result = num1 / num2;
+console.log(`Total character divided by total word in string is: ${result}`);
+
+var result = num1 * num2;
+console.log(`Total character multiply by total word in string is: ${result}`);
