e0733623e1e7e539f99863621b3dfd1dea2cc459
nalandagaikwad78 <nalandagaikwad78@gmail.com>
2023-05-21 21:38:25 +0530

    Operator Ternary assignC
    Operator Ternary assignC

---
+++ 05_operatorTernaryAssigC.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+var boyName = "Billgates";
+var age = 25
+var gender = "Male";
+
+var result = (gender=="Female" && age>=18) || (gender=="Male" && age>=21)  ?  `Hey ${boyName} you are eligible for marriage` :    `Hey ${boyName} you are not eligible for marriage`;
+console.log(`${result}`);
+
+var boyName = "Stew Jobs";
+var age = 17
+var gender = "Male";
+
+var result = (gender=="Female" && age>=18) || (gender=="Male" && age>=21)  ?  `Hey ${boyName} you are eligible for marriage` :  `Hey ${boyName} you are not eligible for marriage`;
+console.log(`${result}`);
+
+var girlName = "Jenifer";
+var age = 16
+var gender = "Female";
+var result = (gender=="Female" && age>=18) || (gender=="Male" && age>=21)  ?  `Hey ${girlName} you are eligible for marriage` :  `Hey ${girlName} you are not eligible for marriage`;
+console.log(`${result}`);
+
+var girlName1 = "Malinda Gates";
+var age = 27
+var gender = "Female";
+
+var result = (gender=="Female" && age>=18) || (gender=="Male" && age>=21)  ?  `Hey ${girlName1} you are eligible for marriage` : `Hey ${girlName1} you are not eligible for marriage`;
+console.log(`${result}`);
