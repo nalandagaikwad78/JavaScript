000309777bbb2c260cb82ede890004a7f8f0125d
nalandagaikwad78 <nalandagaikwad78@gmail.com>
2023-05-21 21:34:47 +0530

    Operator Assignment C
    Operator Assignment C

---
+++ 05_operatorAssC.js
@@ -0,0 +1,29 @@
+
+console.log(`${'-----TCS Interview Eligibility------'}`);
+
+
+var candidateName = "Jerry";
+var gradScore = 80;
+var hscScore = 86;
+var sscScore = 90;
+
+var result = ( gradScore>=70) || ( hscScore>=80) || (sscScore>=90) ? `Congrats ${candidateName} You are eligible for TCS interview.` :` Congrats ${candidateName} Unfortunately you are not eligible for interwiew`;
+console.log(`${result}`);
+
+
+var candidateName = "Mahesh";
+var gradScore = 70;
+var hscScore = 65;
+var sscScore = 55;
+
+var result = ( gradScore>=70) || ( hscScore>=80) || (sscScore>=90) ? `Congrats ${candidateName} You are eligible for TCS interview.` :` Congrats ${candidateName} Unfortunately you are not eligible for interwiew`;
+console.log(`${result}`);
+
+
+var candidateName = "Tom";
+var gradScore = 60;
+var hscScore = 79;
+var sscScore = 88;
+
+var result = ( gradScore>=70) || ( hscScore>=80) || (sscScore>=90) ? `Congrats ${candidateName} You are eligible for TCS interview.` :` Congrats ${candidateName} Unfortunately you are not eligible for interwiew`;
+console.log(`${result}`);
