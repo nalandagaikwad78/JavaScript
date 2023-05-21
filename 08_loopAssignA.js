f55517d66730fb84090025f81bb9fcecbc89f535
nalandagaikwad78 <nalandagaikwad78@gmail.com>
2023-05-21 21:42:53 +0530

    Loop Assignment
    Loop Assignment

---
+++ 08_loopAssignA.js
@@ -0,0 +1,12 @@
+var word = "I am very good IT Developer";
+var count = 0;
+for (let index = 0; index < word.length; index++) {
+    var char = word.charAt(index); // 
+    if (char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u' || 
+        char == 'A' || char == 'E' || char == 'I' || char == 'O' || char == 'U') {
+        console.log(char);
+        count++;
+    }
+    
+}
+console.log(`Count total number of vowels: ${count}`);
