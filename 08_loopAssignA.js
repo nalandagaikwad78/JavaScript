707239c8ffd001d6e5414a007b203b0cb0bc4c9e
nalandagaikwad78 <nalandagaikwad78@gmail.com>
2023-05-25 07:35:39 +0530

    loop Assignment
    loop Assignment

--- 08_loopAssignA.js
+++ 08_loopAssignA.js
@@ -10,3 +10,41 @@ for (let index = 0; index < word.length; index++) {
     
 }
 console.log(`Count total number of vowels: ${count}`);
+
+function sumOfQube(){
+    var sum = 0;
+    for (let index =1; index <=5; index++){ 
+     var result = index * index * index;
+        console.log(`${result}`);
+        sum = sum+result;
+    }
+    console.log(`Sum of Qubes of number is : ${sum}`);
+    }
+    sumOfQube();
+   
+
+// function sumOfQube(){
+// var sum = 0;
+// for (let index =1; index <=5; index++){ 
+//     sum = sum + index * index * index;
+// }
+// console.log(`Sum of Qubes of number is : ${sum}`);
+// }
+// sumOfQube();
+
+
+function oddPositionedChars(givenString){
+    var string = "";
+    for (let index = 0; index <givenString .length; index++) {
+        var char = givenString.charAt(index);
+        if (index%2!=0 && char!=" ") {
+            string = string + char;
+        }
+        
+    }
+    console.log("Odd positioned char are:");
+    console.log(`${string}`);
+   
+}
+oddPositionedChars("Hard work always pays back")
+oddPositionedChars("Soon I will be Angular IT Champ")
