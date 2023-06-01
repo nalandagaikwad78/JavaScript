7f1bc0cb17d001f9401e2c2ec97c70e67bb6597e
nalandagaikwad78 <nalandagaikwad78@gmail.com>
2023-06-01 07:08:14 +0530

    While Assignment 1
    While Assignment 1

---
+++ 06_while_Assignment01.js
@@ -0,0 +1,47 @@
+
+let i = 5; 
+while (i<=15){ 
+    console.log(i); 
+    i=i+1;
+}
+console.log("===========================================");
+
+let k = 50; 
+while (k>=40) {
+   console.log(k);
+   k--;
+}
+console.log("===========================================");
+let j=0; 
+while (j<=15){ 
+if (j%2==!0){
+    console.log(j);
+}
+j++; 
+}
+console.log("===========================================");
+
+let l=0; 
+while (l<=10){ 
+if (l%2==0){
+    console.log(l);
+}
+l++; 
+}
+
+console.log("===========================================");
+
+var table = 5; 
+for (var f = 1; f<=10; f++){
+    console.log(f*table);
+}
+console.log("===========================================");
+
+var table = 10; 
+for (var f = 1; f<=10; f++){
+    console.log(f*table);
+}
+console.log("===========================================");
+var arr = [10, 20, 30, 40, 50, 60, 70,80,90, 100].reverse();
+console.log(`reverse table is:`+ arr);
+
